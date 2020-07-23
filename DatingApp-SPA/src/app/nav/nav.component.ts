c50331b0ce11522102a54d1329740cd86683b37e
militho <militho.blazitho@gmail.com>
2020-07-23 00:41:45 -0500

    End of section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('login is sucessfully');
     }, error => {
-      console.log('Failed to login');
+      console.log(error);
     });
   }
 
